5d878eb5feb148bac140cb545ead7e96c62f5975
damianlopez126 <731765@student.dusd.net>
2022-10-26 09:05:34 -0700

    scriptingproject
    Added our scripting project

---
+++ ScriptingProjectLopezP1/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTempature = 85.0f;
+    float hotLimitTempature = 70.0f;
+    float codLimitTempature = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
